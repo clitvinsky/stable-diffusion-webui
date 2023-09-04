c2951587b2390b3f3933ec8f733892357b31d216
clitvinsky <clitvinsky@vmware.com>
2023-09-04 10:49:35 -0400

    save

--- webui-user.sh
+++ webui-user.sh
@@ -10,7 +10,7 @@
 #clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-export COMMANDLINE_ARGS="--skip-torch-cuda-test --no-half --use-cpu all"
+#export COMMANDLINE_ARGS="--skip-torch-cuda-test --no-half --use-cpu all --upcast-sampling"
 
 # python3 executable
 #python_cmd="python3"
